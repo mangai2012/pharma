c020f99e9795b31fedfaf1078b900d456fbdfbbd
mangai2012 <mangai.venkatesh2012@gmail.com>
2021-01-04 20:53:25 +0530

    zizid=1 second version

--- crad001m2305/src/test/java/com/simpletest/GoogleTest.java
+++ crad001m2305/src/test/java/com/simpletest/GoogleTest.java
@@ -1,5 +1,6 @@
 package com.simpletest;
 //intial version
+//next version from initial version
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
