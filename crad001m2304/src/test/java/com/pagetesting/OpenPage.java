60ea64f85bcbfb753eda19272aa8998f640fb206
admin <admin@192.168.1.8>
2021-01-04 05:57:04 +0530

    initial version of CRAD

---
+++ crad001m2304/src/test/java/com/pagetesting/OpenPage.java
@@ -0,0 +1,26 @@
+package com.pagetesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class OpenPage {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup(){
+	System.setProperty("webdriver.chrome.driver","C:\\Program Files\\Selenium\\drivers\\chromedriver.exe");
+	 driver = new ChromeDriver();
+	
+}
+	@Test
+	public void login(){
+		driver.get("https://www.gmail.com");
+	}
+	@AfterMethod
+	public void teardown(){
+		driver.quit();
+	}
+}
