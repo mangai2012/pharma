a5616cc240c4b8574c058e39f81e893c49d1b9de
admin <admin@192.168.1.8>
2021-01-04 10:50:34 +0530

    intial version

---
+++ crad001m2305/src/test/java/com/simpletest/GoogleTest.java
@@ -0,0 +1,26 @@
+package com.simpletest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup(){
+	System.setProperty("webdriver.chrome.driver","C:\\Program Files\\Selenium\\drivers\\chromedriver.exe");
+	 driver = new ChromeDriver();
+	
+}
+	@Test
+	public void login(){
+		driver.get("https://www.gmail.com");
+	}
+	/*@AfterMethod
+	public void teardown(){
+		driver.quit();
+	}*/
+}
